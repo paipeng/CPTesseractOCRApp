3d4f3530251a5d9869f405ced10d2329af3df274
Pai Peng <sipaipv6@gmail.com>
2024-02-05 10:21:26 +0800

    update

--- src/main/java/com/paipeng/cptesseractocrapp/util/TesseractUtil.java
+++ src/main/java/com/paipeng/cptesseractocrapp/util/TesseractUtil.java
@@ -9,12 +9,15 @@ import net.sourceforge.tess4j.TesseractException;
 public class TesseractUtil {
     public static void decode(String fileName) {
         File imageFile = new File(fileName); //"eurotext.tif"
-        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        ITesseract tesseract = new Tesseract();  // JNA Interface Mapping
         // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
-        instance.setDatapath("tessdata"); // path to tessdata directory
+        tesseract.setDatapath("tessdata"); // path to tessdata directory
 
         try {
-            String result = instance.doOCR(imageFile);
+            tesseract.setLanguage("eng");
+            tesseract.setPageSegMode(1);
+            tesseract.setOcrEngineMode(1);
+            String result = tesseract.doOCR(imageFile);
             System.out.println(result);
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
